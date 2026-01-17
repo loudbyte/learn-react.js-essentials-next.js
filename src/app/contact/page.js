c314a34ad02e1e2e499674a3783d5324757d0e49
LOUDBYTE <loudbyte@users.noreply.github.com>
2026-01-18 00:02:30 +0500

    Add form

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,42 @@
 export default function Page() {
   return (
-    <main>
-      <div>
-        <h1>Contact</h1>
-      </div>
+    <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
+      <h1 className="text-2x1 font-bold text-center md-6">Contact us</h1>
+      <form className="space-y-4">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            required
+            className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <button type="submit" className="text-white bg-blue-600 rounded-md p-3">
+          Send Message
+        </button>
+      </form>
     </main>
   );
 }
